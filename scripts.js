a7464fa4cf46d3bfdb377fbd91b37ff8a2c75e6b
Saranya <sarakrish34@gmail.com>
2022-11-17 20:01:51 -0600

    added a command

--- scripts.js
+++ scripts.js
@@ -2,15 +2,15 @@
 // Remember to pay attention to page loading!
 
 window.addEventListener("load", function() {
-    const takeoffButton = document.getElementId("takeoff")
-    const landingButton = document.getElementId("landing")
-    const abortButton = document.getElementId("missionAbort")
-    
+    const takeoffButton = document.getElementId("takeoff");
+    const landingButton = document.getElementId("landing");
+    const abortButton = document.getElementId("missionAbort");
+    const flightStatus=document.getElementId("flightStatus");
     
     window.addEventListener("click", function(){
         let liftoffConfirm = window.confirm("Confirm that the shuttle is ready for takeoff.")
         if (liftoffConfirm == true){
-            
+            flightStatus.innerHTMl += "Shuttle in flight."
         }
     })
 
