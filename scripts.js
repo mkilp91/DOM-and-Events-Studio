1282df3aa4b3c7ead7463ceceede4285c865e59e
DH001 <dhutchen01@gmail.com>
2022-11-17 19:47:11 -0600

    added some lines

--- scripts.js
+++ scripts.js
@@ -1,4 +1,20 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+
 window.addEventListener("load", function() {
+    const takeoffButton = document.getElementId("takeoff")
+    const landingButton = document.getElementId("landing")
+    const abortButton = document.getElementId("missionAbort")
+    
+    
+    window.addEventListener("click", function(){
+        let liftoffConfirm = window.confirm("Confirm that the shuttle is ready for takeoff.")
+        if (liftoffConfirm == true){
+            
+        }
+    })
+
+
+
+
 });
