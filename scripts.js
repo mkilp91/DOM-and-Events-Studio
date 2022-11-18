66626018cfbad55c50f0eb57ca034afa210baf57
mkilp91 <mitchellakilpatrick@gmail.com>
2022-11-17 20:13:02 -0600

    fixed the elelment method

--- scripts.js
+++ scripts.js
@@ -2,13 +2,13 @@
 // Remember to pay attention to page loading!
 
 window.addEventListener("load", function() {
-    const takeoffButton = document.getElementId("takeoff");
-    const landingButton = document.getElementId("landing");
-    const abortButton = document.getElementId("missionAbort");
-    const flightStatus=document.getElementId("flightStatus");
+    const takeoffButton = document.getElementById("takeoff");
+    const landingButton = document.getElementById("landing");
+    const abortButton = document.getElementById("missionAbort");
+    const flightStatus=document.getElementById("flightStatus");
     
-    window.addEventListener("click", function(){
-        let liftoffConfirm = window.confirm("Confirm that the shuttle is ready for takeoff.")
+    takeoffButton.addEventListener("click", function(){
+        let liftoffConfirm = window.confirm("Confirm that the shuttle is ready for takeoff.");
         if (liftoffConfirm == true){
             flightStatus.innerHTMl += "Shuttle in flight."
         }
